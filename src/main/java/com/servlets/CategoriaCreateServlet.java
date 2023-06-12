30d593d8bbde945aa981f1f5c265ecd2f11b146c
carolos6 <55069690+carolos6@users.noreply.github.com>
2023-06-12 18:19:02 -0500

    Update CategoriaCreateServlet.java

--- src/main/java/com/servlets/CategoriaCreateServlet.java
+++ src/main/java/com/servlets/CategoriaCreateServlet.java
@@ -14,15 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "CategoriaCreateServlet", urlPatterns = {"/CategoriaCreateServlet"})
 public class CategoriaCreateServlet extends HttpServlet {
 
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     response.setContentType("text/html;charset=UTF-8");
