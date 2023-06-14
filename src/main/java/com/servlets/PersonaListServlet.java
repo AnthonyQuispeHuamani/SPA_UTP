fd5ffd084b16a663a1f3784a6a7d66f5f7b43ca0
anthony <116324140+AnthonyQuispeHuamani@users.noreply.github.com>
2023-06-14 12:43:34 -0500

    Cambios en el servlet  PersonalistServlet
    Se hizo un CAMBIO

--- src/main/java/com/servlets/PersonaListServlet.java
+++ src/main/java/com/servlets/PersonaListServlet.java
@@ -5,7 +5,6 @@ import com.controller.PersonaJpaController;
 import com.controller.TipoPersonaJpaController;
 import com.dto.Distrito;
 import com.dto.Persona;
-import com.dto.Telefono;
 import com.dto.TipoPersona;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,105 +20,97 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "PersonaListServlet", urlPatterns = {"/PersonaListServlet"})
 public class PersonaListServlet extends HttpServlet {
 
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
 
-    System.out.println("Entrando a Persona List Servlet");
-    try {
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.spa_utp2023_war_1.0PU");
+        System.out.println("Entrando a Persona List Servlet");
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.spa_utp2023_war_1.0PU");
 
-      PersonaJpaController jpacPersona = new PersonaJpaController(emf);
-      TipoPersonaJpaController jpacTdP = new TipoPersonaJpaController(emf);
-      DistritoJpaController jpacDistrito = new DistritoJpaController(emf);
+            PersonaJpaController jpacPersona = new PersonaJpaController(emf);
+            TipoPersonaJpaController jpacTdP = new TipoPersonaJpaController(emf);
+            DistritoJpaController jpacDistrito = new DistritoJpaController(emf);
 
-      List<Persona> personas = new ArrayList<>();
-      List<TipoPersona> TdPs = new ArrayList<>();
-      List<Distrito> distritos = new ArrayList<>();
+            List<Persona> personas = new ArrayList<>();
+            List<TipoPersona> TdPs = new ArrayList<>();
+            List<Distrito> distritos = new ArrayList<>();
 //      List<Telefono> telefonos = new ArrayList<>();
 
 //      System.out.println(jpacontroller_object.findDistritoEntities());
-      personas = jpacPersona.findPersonaEntities();
-      TdPs = jpacTdP.findTipoPersonaEntities();
-      distritos = jpacDistrito.findDistritoEntities();
+            personas = jpacPersona.findPersonaEntities();
+            TdPs = jpacTdP.findTipoPersonaEntities();
+            distritos = jpacDistrito.findDistritoEntities();
 
-      System.out.println("--------------------Antes del lambda--------------------");
-      personas.forEach(per -> {
-        System.out.print(per.getId() + ": " + per.getNombres() + ": " + per.getEmail() + ": ");
-        per.getTelefonoList().forEach(tel -> System.out.print("Telefonos: " + tel.getDescripcion() + ", "));
-        System.out.println();
-      });
-      System.out.println("--------------------Después del Lambda--------------------");
-
-      for (Persona per : personas) {
-        System.out.print(per.getId() + ": " + per.getNombres() + ": " + per.getEmail());
-
-//        Reemplazando foreach por expresion Lambda
-        per.getTelefonoList().forEach(p -> System.out.print(" - Telefono: " + p.getDescripcion()));
-//        for (Telefono tel : per.getTelefonoList()) {
-//          System.out.println(" - Teléfono: " + tel.getDescripcion());
-//        }
-      }
+            personas.forEach(p -> System.out.println("Data: " + p.getId() + " - " + p.getNombres()));
+            //Expresion lambda reemplazando foreach
+//            for (Persona per : personas) {
+//                System.out.print(per.getId() + ": " + per.getNombres() + ": " + per.getEmail());
+//            }
 //      for (TipoPersona tipoPer : TdPs) {
 //        System.out.println(tipoPer.getId() + ": " + tipoPer.getDescripcion());
 //      }
+            for (Persona pe : personas) {
+                pe.getComprobanteList().forEach(d -> System.out.println(d.getNumero()));
+            }
 
-      TdPs.forEach(p -> System.out.println(p.getId() + " - " + p.getDescripcion()));
+            TdPs.forEach(p -> System.out.println(p.getId() + " - " + p.getDescripcion()));
 
-      request.setAttribute("mi_lista_de_personas", personas);
-      request.setAttribute("mi_lista_de_TdP", TdPs);
-      request.setAttribute("miListaDeDistritos", distritos);
-      request.getRequestDispatcher("Persona.jsp").forward(request, response);
-    } catch (IOException | ServletException theException) {
-      System.out.println(theException);
+            request.setAttribute("mi_lista_de_personas", personas);
+            request.setAttribute("mi_lista_de_TdP", TdPs);
+            request.setAttribute("miListaDeDistritos", distritos);
+            request.getRequestDispatcher("Persona.jsp").forward(request, response);
+        } catch (IOException | ServletException theException) {
+            System.out.println(theException);
+        }
     }
-  }
 
-  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-  /**
-   * Handles the HTTP <code>GET</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-  /**
-   * Handles the HTTP <code>POST</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-  /**
-   * Returns a short description of the servlet.
-   *
-   * @return a String containing servlet description
-   */
-  @Override
-  public String getServletInfo() {
-    return "Short description";
-  }// </editor-fold>
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 }
